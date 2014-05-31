aa9436de8dde4606ad141295e317c6b483deb63b
Justin Grevich <justin@grevi.ch>
2014-05-31 01:47:21 -0700

    Add env specific run lists to app_server role

--- kitchen/roles/app_server.rb
+++ kitchen/roles/app_server.rb
@@ -1,3 +1,4 @@
 name "app-server"
 description "The app server role for boxit"
 run_list ["passenger::install", "passenger::daemon"]
+#env_run_lists "production" => ["recipe[passenger::config_prod]"], "testing" => ["recipe[passenger::config_test]"]
